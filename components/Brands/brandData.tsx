fbd633f83dea5a2fc220470551a25f775b17883f
Thisisashutosh <thisisashutoshdubey@gmail.com>
2024-03-08 12:32:19 +0530

    first commit

---
+++ components/Brands/brandData.tsx
@@ -0,0 +1,48 @@
+import { Brand } from "@/types/brand";
+
+const brandData: Brand[] = [
+  {
+    id: 0.25,
+    name: "Client",
+    href: "#",
+    image: "/images/brand/sida.svg",
+    imageLight: "/images/brand/sida.svg",
+  },
+  {
+    id: 0.3,
+    name: "Client",
+    href: "#",
+    image: "/images/brand/sida.svg",
+    imageLight: "/images/brand/sida.svg",
+  },
+  {
+    id: 0.4,
+    name: "Client",
+    href: "#",
+    image: "/images/brand/sida.svg",
+    imageLight: "/images/brand/sida.svg",
+  },
+  {
+    id: 0.5,
+    name: "Client",
+    href: "#",
+    image: "/images/brand/sida.svg",
+    imageLight: "/images/brand/sida.svg",
+  },
+  {
+    id: 0.6,
+    name: "Client",
+    href: "#",
+    image: "/images/brand/sida.svg",
+    imageLight: "/images/brand/sida.svg",
+  },
+  {
+    id: 0.7,
+    name: "Client",
+    href: "#",
+    image: "/images/brand/sida.svg",
+    imageLight: "/images/brand/sida.svg",
+  },
+];
+
+export default brandData;
---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,47 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "https://tiimg.tistatic.com/fp/1/007/507/paracetamol-fast-release-crocin-advance-tablet-500-mg-for-mild-to-moderate-pain--234.jpg",
+    title: "Paracetamol",
+   
+  },
+  {
+    id: 2,
+    icon: "https://5.imimg.com/data5/SELLER/Default/2023/6/319597573/MH/NE/SR/135658020/ibuprofen-400-mg-bp-tablets.jpg",
+    title: "Ibuprofen",
+  },
+  {
+    id: 3,
+    icon: "https://www.benadryl.ca/sites/benadryl_ca/files/styles/product_image/public/ben_062600963635_allergy_caplets_36ct_en.jpg",
+    title: "Antihistamine",
+  },
+  {
+    id: 4,
+    icon: "https://5.imimg.com/data5/SELLER/Default/2023/7/330506870/UM/GZ/QO/135658020/aspirin-dispersible-tablets.jpg",
+    title: "Aspirin",
+  },
+  {
+    id: 5,
+    icon: "https://cdn01.pharmeasy.in/dam/products_otc/Z63893/liveasy-wellness-antacid-mint-flavour-sugar-free-bottle-of-200ml-oral-liquid-2-1695207631.jpg",
+    title: "Antacid",
+  },
+  {
+    id: 6,
+    icon: "https://m.media-amazon.com/images/I/61ildyzNgcL.jpg",
+    title: "Dettol",
+  },
+  {
+    id: 7,
+    icon: "https://www.lyfechemist.com/wp-content/uploads/2022/09/Pan-40-Mg-tablet-.jpg",
+    title: "Pan 40",
+  },
+  {
+    id: 8,
+    icon: "https://5.imimg.com/data5/SELLER/Default/2023/1/MA/BO/QE/977131/digital-thermometer-500x500.jpeg",
+    title: "Thermometer",
+  },
+];
+
+export default featuresData;
