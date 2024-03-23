b17c46c66ed6d9feecacaf8775a765bb32f035f6
Thisisashutosh <thisisashutoshdubey@gmail.com>
2024-03-23 21:11:41 +0530

    adjusted categories section

--- components/Categories/categoriesData.tsx
+++ components/Categories/categoriesData.tsx
@@ -1,45 +1,37 @@
+import diabetes from "../../public/images/icon/diabetes.svg"
+
 const categoriesData: any = [
   {
     id: 1,
-    icon: "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/105ceca515ee30509f93309ae75306aa.png?f=png?dim=256x0",
+    icon: {diabetes},
     title: "Diabetic Care",
    
   },
-  {
-    id: 2,
-    icon: "https://images.apollo247.in/pub/media/catalog/category/babycare_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
-    title: "Maternity Care",
-  },
-  {
-    id: 3,
-    icon: "https://images.apollo247.in/pub/media/catalog/category/womencare_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
-    title: "Feminine Hygiene",
-  },
-  {
-    id: 4,
-    icon: "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ce48d6d71ace3892945f0b0a64c1f0e9.png?f=png?dim=256x0",
-    title: "Skin Care",
-  },
-  {
-    id: 5,
-    icon: "https://images.apollo247.in/pub/media/catalog/category/personalcare_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
-    title: "Personal Care",
-  },
-  {
-    id: 6,
-    icon: "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/35d6d4e00e5d3058906cfa8b92ca799c.png?f=png?dim=256x0",
-    title: "Health Care",
-  },
-  {
-    id: 7,
-    icon: "https://images.apollo247.in/pub/media/catalog/category/healthdevices_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
-    title: "Healthcare Devices",
-  },
-  {
-    id: 8,
-    icon: "https://images.apollo247.in/pub/media/catalog/category/sexual_health_wellness_new.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
-    title: "Sexual Health",
-  },
+  // {
+  //   id: 2,
+  //   icon: "https://images.apollo247.in/pub/media/catalog/category/babycare_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
+  //   title: "Maternity Care",
+  // },
+  // {
+  //   id: 3,
+  //   icon: "https://images.apollo247.in/pub/media/catalog/category/womencare_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
+  //   title: "Feminine Hygiene",
+  // },
+  // {
+  //   id: 4,
+  //   icon: "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ce48d6d71ace3892945f0b0a64c1f0e9.png?f=png?dim=256x0",
+  //   title: "Skin Care",
+  // },
+  // {
+  //   id: 5,
+  //   icon: "https://images.apollo247.in/pub/media/catalog/category/personalcare_2.jpg?tr=w-367.5,q-100,f-webp,c-at_max",
+  //   title: "Personal Care",
+  // },
+  // {
+  //   id: 6,
+  //   icon: "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/35d6d4e00e5d3058906cfa8b92ca799c.png?f=png?dim=256x0",
+  //   title: "Health Care",
+  // },
 ];
 
 export default categoriesData;
